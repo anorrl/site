03036ec70b8fdcd890cda432d7b557df6a244a53
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2026-05-26 15:44:43 +0100

    triple oops

--- private/lib/anorrl/Datastore.php
+++ private/lib/anorrl/Datastore.php
@@ -56,7 +56,7 @@
 
 			while($continue) {
 				$result = Database::singleton()->run(
-					"SELECT `target`, `value` FROM `datastores` WHERE `universe` = :universe AND `scope`=:scope AND `key`=:key AND `type` = :type LIMIT :page, :pagesize ORDER BY `value` ".($ascending ? "ASC" :"DESC"),
+					"SELECT `target`, `value` FROM `datastores` WHERE `universe` = :universe AND `scope`=:scope AND `key`=:key AND `type` = :type ORDER BY `value` ".($ascending ? "ASC" :"DESC"). "LIMIT :page, :pagesize",
 					[
 						"key" => $key,
 						"universe" => $this->universe->id,
