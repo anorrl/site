fd2d73c22c5811b94a11cabcc7991afbcc3042af
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2026-07-01 08:28:38 +0100

    frontend rewrite basically done?

--- private/views/index2.php
+++ private/views/index2.php
@@ -27,12 +27,59 @@
 	#newfrontpage table form {
 		text-align: center;
 	}
+
+	.fieldset input {
+		border: 2px solid rgb(141, 29, 216);
+		background: #482b5a;
+		color:beige;
+		margin: 5px;
+		padding: 2px 5px;
+	}
+
+	.panel {
+		padding: 5px;
+		
+	}
+
+	.panel input[type="submit"] {
+		border: 2px solid rgb(141, 29, 216);
+		font-size: 14px;
+		padding: 5px 15px;
+		font-weight: bold;
+		background: linear-gradient(180deg,rgb(156, 55, 223) 0%, rgb(81, 34, 112) 100%);
+		color: white;
+		cursor: pointer;
+	}
+
+	.panel input[type="submit"]:hover {
+		filter: brightness(1.15);
+	}
+
+	#auth[data-selected="register"] a[href="javascript:openRegisterPanel()"],
+	#auth[data-selected="login"] a[href="javascript:openLoginPanel()"] {
+		font-size: 16px;
+		color: #fff;
+		text-decoration: underline;
+	}
+
+	.helperfield {
+		font-style:italic;
+		font-size: 10px;
+		color: #bbb;
+	}
+
+	h3 a, h3 a:hover {
+		color: #cbc;
+		font-size: 13px;
+	}
+
 </style>
 <script>
 	function openLoginPanel() {
 		if(!$("#login").is(":visible")) {
 			$("#login").show();
 			$("#register").hide();
+			$("#auth").attr("data-selected", "login")
 		}
 	}
 
@@ -40,23 +87,31 @@
 		if(!$("#register").is(":visible")) {
 			$("#register").show();
 			$("#login").hide();
+			$("#auth").attr("data-selected", "register")
 		}
 	}
+
+	$(function() {
+		$("#login").show();
+			$("#register").hide();
+			$("#auth").attr("data-selected", "login")
+	})
 </script>
 <div id="newfrontpage">
 	<img src="/public/images/header/logo.png" height="225">
 	<table>
 		<tr>
 			<td>
-				<div class="box">
-					<h1>Awesome box</h1>
-					<p>(add characters!!!)</p>
+				<div class="box" style="position: relative">
+					<img src="/public/images/test.jpg" height="300">
+					<img src="/public/images/anorrl-fellas1.png" style="width: 245px; position: absolute;left: -56px;bottom: -62px;">
 				</div>
 			</td>
-			<td width="50%">
-				<div class="box">
+			<td width="25%">
+				<div class="box" id="auth">
 					<h3><a href="javascript:openRegisterPanel()">Register</a> | <a href="javascript:openLoginPanel()">Login</a></h3>
 					<div id="login" class="panel">
+						<h3 style="margin-top: 0px;">welcome back!</h3>
 						<form method="POST">
 							<div class="fieldset">
 								<input type="text" placeholder="Username">
@@ -69,17 +124,24 @@
 					</div>
 					<div id="register" class="panel" style="display: none">
 						<form method="POST">
+							<h3 style="margin-top: 0px;">are you new around here?</h3>
 							<div class="fieldset">
 								<label>Username</label>
-								<input type="text" placeholder="thats your name right?">
+								<input style="margin-bottom: 0px;" type="text" placeholder="thats your name right?" minlength="3" maxlength="20" required>
+								<label class="helperfield"> 3-20 alphanumeric characters, no spaces </label>
 							</div>
 							<div class="fieldset">
 								<label>Password</label>
-								<input type="password" placeholder="thats your password right?">
+								<input style="margin-bottom: 0px;" type="password" placeholder="thats your password right?" minlength="6" maxlength="20" required>
+								<label class="helperfield"> 6-20 characters, min 4 letters & 2 numbers </label>
 							</div>
 							<div class="fieldset">
 								<label>Confirm Password</label>
-								<input type="password" placeholder="just in case... do it again...">
+								<input type="password" placeholder="just in case... do it again..."  minlength="6" maxlength="20" required>
+							</div>
+							<div class="fieldset">
+								<label>Invite Key</label>
+								<input type="password" placeholder="sigh... you know the deal..." required>
 							</div>
 							<input type="submit" value="Register">
 						</form>
